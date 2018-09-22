bdbdde8d99990aac67f04afb63dc64436ee03396
liangziai77 <32543205+liangziai77@users.noreply.github.com>
2018-09-23 07:58:26 +0800

    Update gz.js

--- xcss/gz.js
+++ xcss/gz.js
@@ -4,7 +4,7 @@ function install(id,obj){
   //var url="https://workflow.is/workflows/"+id
   //window.open(url)
   if(id=="1"){
-	 var url="workflow://import-workflow/?url=https%3A%2F%2Fcvws.icloud-content.com%2FB%2FAc9teMi9urcsfD7Iwbv4a27mU-lX%2F%24%257Bf%257D%3Fo%3DAt5wb_y1rozWEmC9CinNQn0PnLRQ5MvnrAwEpbG0r1UbIIatjGeA4SZWGoj6FA4WYQ%26v%3D1%26x%3D3%26a%3DB4yOkk4ayLyfnGYTzS8_OJ_9TKnJA0UDRgEAAAMJA0Y%26e%3D1537605400%26k%3D_%26fl%3D%26r%3De2cce8c2-0fd2-4118-90c5-5d7ae14236c6-1%26ckc%3Dcom.apple.shortcuts%26ckz%3D_defaultZone%26p%3D33%26s%3Dm3A7Ah_JHNCnJ1naX0_dETyQS-g&name=%E5%B0%8F%E8%89%AF%20-%20%E6%9B%B4%E6%96%B0%E5%99%A8%203.3"
+	 var url="workflow://import-workflow/?url=https%3A%2F%2Fgitee.com%2Fyao07%2Fupdate_device%2Fraw%2Fmaster%2Fxiaoliao3.3&name=%E5%B0%8F%E8%89%AF%20-%20%E6%9B%B4%E6%96%B0%E5%99%A8%203.3"
     window.open(url)
   }else{
 	var url="https://workflow.is/workflows/"+id
