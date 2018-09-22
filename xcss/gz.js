df5f64a2fd57d8fa4198bd4882db977a5dfa9031
liangziai77 <32543205+liangziai77@users.noreply.github.com>
2018-09-22 08:58:01 +0800

    Update gz.js

--- xcss/gz.js
+++ xcss/gz.js
@@ -1,5 +1,15 @@
 function install(id,obj){
-  var divs = obj.getElementsByTagName("a");
-  var url="workflow://import-workflow/?url=https%3A%2F%2Fworkflow.is%2Fworkflows%2F"+id+".wflow&name="+encodeURI(divs[0].innerHTML)
+  //var divs = obj.getElementsByTagName("a");
+  //var url="workflow://import-workflow/?url=https%3A%2F%2Fworkflow.is%2Fworkflows%2F"+id+".wflow&name="+encodeURI(divs[0].innerHTML)
+  var url="https://workflow.is/workflows/"+id
   window.open(url)
+  if(id=="0a228fe5a0204f9196e0dfc10058777e"){
+	  alert("更新器正在更新中···\n请稍后添加")
+  }else{
+	var url="https://workflow.is/workflows/"+id
+    window.open(url)
+  }
+  
+ // var ua = navigator.userAgent.toLowerCase();
+  //console.log(ua)
 }
