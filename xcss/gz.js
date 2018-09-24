3415b01b2978f23d6a8c39181d89637a200ee122
liangziai77 <32543205+liangziai77@users.noreply.github.com>
2018-09-24 20:24:23 +0800

    Update gz.js

--- xcss/gz.js
+++ xcss/gz.js
@@ -3,7 +3,10 @@ function install(id,obj){
   //var url="workflow://import-workflow/?url=https%3A%2F%2Fworkflow.is%2Fworkflows%2F"+id+".wflow&name="+encodeURI(divs[0].innerHTML)
   //var url="https://workflow.is/workflows/"+id
   //window.open(url)
-  if(id=="1"){
+if(id==“2”){
+var url="workflow://import-workflow/?url=https%3A%2F%2Fgitee.com%2Fyao07%2Fupdate_device%2Fraw%2Fmaster%2F%25E6%258D%25B7%25E5%25BE%2584%25E9%2593%25BE%25E6%258E%25A5%25E8%25BD%25ACWorkflow%25E9%2593%25BE%25E6%258E%25A5&name=%E6%8D%B7%E5%BE%84%E9%93%BE%E6%8E%A5%E8%BD%ACWorkflow%E9%93%BE%E6%8E%A5"
+window.open(url)
+}else if(id=="1"){
 	 var url="workflow://import-workflow/?url=https%3A%2F%2Fgitee.com%2Fyao07%2Fupdate_device%2Fraw%2Fmaster%2Fxiaoliao3.3&name=%E5%B0%8F%E8%89%AF%20-%20%E6%9B%B4%E6%96%B0%E5%99%A8%203.3"
     window.open(url)
   }else{
