6867914c13135310d71b07122256592e2b4ed36c
liangziai77 <32543205+liangziai77@users.noreply.github.com>
2018-10-09 14:38:25 +0800

    Add files via upload

--- xcss/gz.js
+++ xcss/gz.js
@@ -1,9 +1,13 @@
+
 function install(id,obj){
   //var divs = obj.getElementsByTagName("a");
   //var url="workflow://import-workflow/?url=https%3A%2F%2Fworkflow.is%2Fworkflows%2F"+id+".wflow&name="+encodeURI(divs[0].innerHTML)
   //var url="https://workflow.is/workflows/"+id
   //window.open(url)
-if(id=="2"){
+if(id=="3"){
+var url="workflow://import-workflow/?url=https%3A%2F%2Fgitee.com%2Fyao07%2Fupdate_device%2Fraw%2Fmaster%2F%25E8%25A7%2584%25E5%2588%2599%2FWorkflow%25E8%25A7%2584%25E5%2588%2599%25E5%25AE%2589%25E8%25A3%2585%25E5%2599%25A8&name=Workflow%E8%A7%84%E5%88%99%E5%AE%89%E8%A3%85%E5%99%A8"
+window.open(url)
+}else if(id=="2"){
 var url="workflow://import-workflow/?url=https%3A%2F%2Fgitee.com%2Fyao07%2Fupdate_device%2Fraw%2Fmaster%2F%25E6%258D%25B7%25E5%25BE%2584%25E9%2593%25BE%25E6%258E%25A5%25E8%25BD%25ACWorkflow%25E9%2593%25BE%25E6%258E%25A5&name=%E6%8D%B7%E5%BE%84%E9%93%BE%E6%8E%A5%E8%BD%ACWorkflow%E9%93%BE%E6%8E%A5"
 window.open(url)
 }else if(id=="1"){
@@ -17,3 +21,4 @@ window.open(url)
  // var ua = navigator.userAgent.toLowerCase();
   //console.log(ua)
 }
+
