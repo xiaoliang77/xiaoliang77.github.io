9d60f3163623e476947c3688fb964ce1425ea4bd
liangziai77 <32543205+liangziai77@users.noreply.github.com>
2018-10-09 16:19:26 +0800

    Add files via upload

--- xcss/gz.js
+++ xcss/gz.js
@@ -4,11 +4,11 @@ function install(id,obj){
   //var url="workflow://import-workflow/?url=https%3A%2F%2Fworkflow.is%2Fworkflows%2F"+id+".wflow&name="+encodeURI(divs[0].innerHTML)
   //var url="https://workflow.is/workflows/"+id
   //window.open(url)
-if(id=="3"){
+if(id=="2"){
 var url="workflow://import-workflow/?url=https%3A%2F%2Fgitee.com%2Fyao07%2Fupdate_device%2Fraw%2Fmaster%2F%25E8%25A7%2584%25E5%2588%2599%2FWorkflow%25E8%25A7%2584%25E5%2588%2599%25E5%25AE%2589%25E8%25A3%2585%25E5%2599%25A8&name=Workflow%E8%A7%84%E5%88%99%E5%AE%89%E8%A3%85%E5%99%A8"
 window.open(url)
-}else if(id=="2"){
-var url="workflow://import-workflow/?url=https%3A%2F%2Fgitee.com%2Fyao07%2Fupdate_device%2Fraw%2Fmaster%2F%25E6%258D%25B7%25E5%25BE%2584%25E9%2593%25BE%25E6%258E%25A5%25E8%25BD%25ACWorkflow%25E9%2593%25BE%25E6%258E%25A5&name=%E6%8D%B7%E5%BE%84%E9%93%BE%E6%8E%A5%E8%BD%ACWorkflow%E9%93%BE%E6%8E%A5"
+}else if(id=="3"){
+var url="workflow://import-workflow/?url=https%3A%2F%2Fgitee.com%2Fyao07%2Fupdate_device%2Fraw%2Fmaster%2F%25E8%25A7%2584%25E5%2588%2599%2FYouTube%25E8%25A7%2586%25E9%25A2%2591%25E4%25B8%258B%25E8%25BD%25BD%0A&name=YouTube%E8%A7%86%E9%A2%91%E4%B8%8B%E8%BD%BD"
 window.open(url)
 }else if(id=="1"){
 	 var url="workflow://import-workflow/?url=https%3A%2F%2Fgitee.com%2Fyao07%2Fupdate_device%2Fraw%2Fmaster%2Fxiaoliao3.3&name=%E5%B0%8F%E8%89%AF%20-%20%E6%9B%B4%E6%96%B0%E5%99%A8%203.3"
