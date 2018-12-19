a0a57a3f879332d04e117798506f9f68a9bb5b61
84088289 <84088289@qq.com>
2018-12-19 11:42:58 +0800

    fb

---
+++ pin/jb/1024sp.js
@@ -0,0 +1,122 @@
+/*
+脚本仅供代码学习，请勿分享。非法传播照成法律问题与作者无关。
+
+by：iPhone 8、小良
+http://ae85.cn/
+*/
+
+
+$cache.set("id", "111")
+$cache.set("pg", 1)
+var urlt = "http://n2.lufi99.club/"
+var data = [{ "name": "亚洲视频", "id": "111" }, { "name": "日本AV", "id": "112" }, { "name": "欧美电影", "id": "113" },]
+
+$ui.render({
+    props: {
+        title: "1024视频"
+    },
+    views: [{
+        type: "menu",
+        props: {
+            id: "meun",
+            items: data.map(function (item) {
+                return item.name
+            }),
+        },
+        layout: function (make) {
+            make.left.top.right.equalTo(0)
+            make.height.equalTo(50)
+
+        },
+        events: {
+            changed: function (sender) {
+                $cache.set("id", data[sender.index].id)
+                $cache.set("pg", 1)
+                getdata()
+            }
+        }
+    },
+    {
+        type: "list",
+        layout: function (make) {
+            make.right.left.bottom.inset(0)
+            make.top.equalTo($("meun").bottom)
+        },
+        events: {
+            didSelect: function (sender, indexPath, data) {
+                var id = data.split("\n")
+                geting(id[1], id[0])
+            },
+            didReachBottom: function (sender) {
+                sender.endFetchingMore()
+                var page = $cache.get("pg") + 1
+                $cache.set("pg", page)
+                getdata()
+            }
+        }
+
+    },
+    ]
+
+})
+
+function getdata() {
+    var id = $cache.get("id")
+    var pg = $cache.get("pg")
+    $ui.loading(true)
+    $http.get({
+        url: urlt + "pw/thread-htm-fid-" + id + "-page-" + pg + ".html",
+        handler: function (resp) {
+            $ui.loading(false)
+            var text = resp.data.replace(/\n|\s|\r/g, "")
+            if (text.indexOf('普通主题') !== -1) {
+                text = text.split("普通主题")[1]
+            }
+            var shu = text.match(/class=\"tr3t_one\">(\S*?)<\/h3>/g)
+            if (pg == 1) {
+                var data = []
+            } else {
+                var data = $("list").data
+            }
+            for (i in shu) {
+                var a = shu[i]
+                if (a.indexOf('href=') !== -1) {
+                    var txt = a.split("<h3>")[1]
+                    var mc = txt.match(/\">(\S*?)<\/a>/)[1]
+                    var id = a.match(/href=\"(\S*?)\"/)[1]
+                    data.push(mc + "\n" + id)
+                }
+            }
+            $("list").data = data
+            $("list").endFetchingMore()
+        }
+    })
+}
+
+getdata()
+
+function geting(id, mc) {
+    $ui.loading(true)
+    $http.get({
+        url: urlt + "pw/" + id,
+        handler: function (resp) {
+            $ui.loading(false)
+            var text = resp.data.replace(/\n|\s|\r/g, "")
+            var url = text.match(/<divclass=\"f14\".*?<\/div>/)[0]
+            var u = url.replace(/&#46;/g, '.')
+            url = $detector.link(u)[0];
+            $ui.push({
+                props: {
+                    title: mc
+                },
+                views: [{
+                    type: "web",
+                    props: {
+                        url: url,
+                    },
+                    layout: $layout.fill
+                }]
+            })
+        }
+    })
+}
---
+++ pin/jb/1024xs.js
@@ -0,0 +1,121 @@
+/*
+脚本仅供代码学习，请勿分享。非法传播照成法律问题与作者无关。
+
+by：iPhone 8、小良
+http://ae85.cn/
+*/
+
+
+$cache.set("id", "7")
+$cache.set("pg", 1)
+var urlt = "http://n2.lufi99.club/"
+var data = [{ "name": "激情", "id": "1" }, { "name": "家庭", "id": "2" }, { "name": "校园", "id": "3" }, { "name": "武侠", "id": "4" }, { "name": "明星", "id": "5" }, { "name": "人妻", "id": "6" },]
+
+$ui.render({
+    props: {
+        title: "1024小说"
+    },
+    views: [{
+        type: "menu",
+        props: {
+            id: "meun",
+            items: data.map(function (item) {
+                return item.name
+            }),
+        },
+        layout: function (make) {
+            make.left.top.right.equalTo(0)
+            make.height.equalTo(50)
+
+        },
+        events: {
+            changed: function (sender) {
+                $cache.set("id", data[sender.index].id)
+                $cache.set("pg", 1)
+                getdata()
+            }
+        }
+    },
+    {
+        type: "list",
+        layout: function (make) {
+            make.right.left.bottom.inset(0)
+            make.top.equalTo($("meun").bottom)
+        },
+        events: {
+            didSelect: function (sender, indexPath, data) {
+                var id = data.split("\n")
+                geting(id[1], id[0])
+            },
+            didReachBottom: function (sender) {
+                sender.endFetchingMore()
+                var page = $cache.get("pg") + 1
+                $cache.set("pg", page)
+                getdata()
+            }
+        }
+
+    },
+    ]
+
+})
+
+function getdata() {
+    var id = $cache.get("id")
+    var pg = $cache.get("pg")
+    $ui.loading(true)
+    $http.get({
+        url: urlt + "pw/thread-htm-fid-17-type-" + id + "-page-" + pg + ".html",
+        handler: function (resp) {
+            $ui.loading(false)
+            var text = resp.data.replace(/\n|\s|\r/g, "")
+            if (text.indexOf('普通主题') !== -1) {
+                text = text.split("普通主题")[1]
+            }
+            var shu = text.match(/class=\"tr3t_one\">(\S*?)<\/h3>/g)
+            if (pg == 1) {
+                var data = []
+            } else {
+                var data = $("list").data
+            }
+            for (i in shu) {
+                var a = shu[i]
+                if (a.indexOf('href=') !== -1) {
+                    var txt = a.split("<h3>")[1]
+                    var mc = txt.match(/\">(\S*?)<\/a>/)[1]
+                    var id = a.match(/href=\"(\S*?)\"/)[1]
+                    data.push(mc + "\n" + id)
+                }
+            }
+            $("list").data = data
+            $("list").endFetchingMore()
+        }
+    })
+}
+
+getdata()
+
+function geting(id, mc) {
+    $ui.loading(true)
+    $http.get({
+        url: urlt + "pw/" + id,
+        handler: function (resp) {
+            $ui.loading(false)
+            var text = resp.data.match(/id=\"read_tpc\">.*?<\/div>/)
+            text = text[0].replace('id=\"read_tpc\">', "")
+            var html = `<html><head><meta charset="UTF-8"><title>${mc}</title><style> body,div{ font-size:42px;} </style> </head><body><div>${text}</body></html>`
+            $ui.push({
+                props: {
+                    title: mc
+                },
+                views: [{
+                    type: "web",
+                    props: {
+                        html: html,
+                    },
+                    layout: $layout.fill
+                }]
+            })
+        }
+    })
+}
