4f642282f0ae1a7b9c07efb03dc281a14fa01d2b
84088289 <84088289@qq.com>
2018-12-23 12:27:17 +0800

    gx

--- pin/jb/dspxzq.js
+++ pin/jb/dspxzq.js
@@ -1,34 +1,119 @@
 /*
-短视频下载器 1.2
-2018年10月30日 更新
-修复不能下载问题。
-支持 jsbox 同时也支持 pin 
+短视频下载器 2.0
+12月23日  全新改版 支持 jsbox 同时也支持 pin 
 
-脚本已支持：皮皮虾、抖音、快手、火山、今日头条、西瓜视频、微博、秒拍、小咖秀、晃咖、微视、美拍、网易云音乐、陌陌、映客、小影 等平台的视频。
+支持：皮皮虾、抖音、快手、火山、今日头条、西瓜视频、微博、秒拍、小咖秀、晃咖、微视、美拍、网易云音乐、陌陌、映客、小影 等平台的视频。
+部分支持去水印下载，如：抖音，快手等！
+
+脚本使用密码到在公众号【小良科技】中回复〖285〗即可获取。
 
 by：iPhone 8、小良
 更多js脚本： http://ae85.cn/
-
 */
-var all_type = [
-  { name: "抖音短视频去水印下载", vid: "douyin" },
-  { name: "快手短视频去水印下载", vid: "kuaishou" },
-  { name: "火山短视频去水印下载", vid: "huoshan" },
-  { name: "头条西瓜视频下载", vid: "toutiao" },
-  { name: "微博、秒拍、小咖秀、晃咖视频下载", vid: "weibo" },
-  { name: "微视短视频去水印下载", vid: "weishi" },
-  { name: "美拍短视频下载", vid: "meipai" },
-  { name: "网易云音乐视频下载", vid: "yunyinyue" },
-  { name: "皮皮虾视频去水印下载", vid: "pipixia" },
-  { name: "陌陌视频下载", vid: "momo" },
-  { name: "映客视频下载", vid: "inke" },
-  { name: "小影小视频下载", vid: "xiaoying" }
-];
+const base64 = "aHR0cHM6Ly9naXRlZS5jb20veLZvMDcvdXBkYXRlX2RldmljZS9yYXcvbLZzdGVyL2R1YW5zaGlwaW5nLmpzb24="
+$ui.loading(true)
+$http.get({
+  url: $text.base64Decode(base64.replace(/LZ/g, "WF")),
+  handler: function (resp) {
+    $ui.loading(false)
+    if (resp.response.statusCode == "200") {
+      var info = resp.data;
+      $cache.set("info", info)
+      if (info.bb != "2.0") {
+        $ui.alert({
+          title: "温馨提示",
+          message: info.gxsm,
+          actions: [{
+            title: "进入官网",
+            handler: function () {
+              $app.openURL(info.gw)
+            }
+          }, {
+            title: "关注公众号",
+            handler: function () {
+              $ui.alert({
+                title: "温馨提示",
+                message: "跳转微信会自动复制公众号ID\n请跳转到微信-搜索-公用号-粘贴-关注",
+                actions: [{
+                  title: "跳转微信",
+                  handler: function () {
+                    $clipboard.text = "ae85-cn"
+                    $app.openURL("weixin://")
+                  }
+                }, {
+                  title: "取消"
+                }]
+              })
+            }
+          }]
+        })
+      } else {
+        yanz()
+
+      }
+
+    } else {
+      $ui.alert("加载失败")
+    }
+  }
+})
+function yanz() {
+  var file = $file.read("key.txt");
+  if (!file) {
+    cshyz();
+  } else {
+    if (yzmm(file.string)) {
+      zjm();
+      tcgg()
+      getclipboard()
+    } else {
+      cshyz();
+    }
+  }
+}
+function tcgg() {
+  var gg = $cache.get("info").gg
+  if ($file.exists("gg.txt")) {
+    var file = $file.read("gg.txt").string
+    if (file != gg) {
+      xrwj(gg)
+    }
+  } else {
+    xrwj(gg)
+  }
+}
+function xrwj(nr) {
+  $ui.alert(nr);
+  $file.write({
+    data: $data({ string: nr }),
+    path: "gg.txt"
+  })
+}
+function getclipboard() {
+  var url = $clipboard.link
+  if (url) {
+    $ui.alert({
+      title: "要提取剪贴板中的链接",
+      message: $clipboard.text,
+      actions: [{
+        title: "取消",
+        handler: function () {
+        }
+      }, {
+        title: "提取",
+        handler: function () {
+          $('bjk').text = url
+          tmenu(url)
+        }
+      }]
+    })
+  }
+}
 
 function cshyz() {
   $ui.render({
     props: {
-      title: "短视频下载器"
+      title: "短视频下载器 2.0"
     },
     views: [
       {
@@ -99,7 +184,7 @@ function cshyz() {
         props: {
           id: "webyz",
           toolbar: true,
-          html: `<head><meta charset="UTF-8"></head><body><span style="font-size:34px;"><h1>使用说明：</h1><h2>为了防止脚本和谐速度过快，首次使用脚本，需要开启密码。</h2><h1>密码获取：</h1><h2>前往公众号【VIP视频解析】在聊天窗口中回复〖285〗即可获取。<br></h2><br><h1><a href="http://t.cn/Evovggo">点击观看</a> 视频教程</h1> <h1><a href="http://ae85.cn/fuli/xiaoliang.html">点击领取</a> 支付宝红包</h1><h1><a href="http://ae85.cn/lxfs.html">关注公众号</a> VIP视频解析</h1><h2>by：iPhone 8、小良&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="http://ae85.cn/">http://ae85.cn/</a> </h2></span></body>`
+          html: `<head><meta charset="UTF-8"></head><body><span style="font-size:34px;"><h1>使用说明：</h1><h2>为了防止脚本和谐速度过快，首次使用脚本，需要开启密码。</h2><h1>密码获取：</h1><h2>前往公众号【小良科技】在聊天窗口中回复〖285〗即可获取。<br></h2><br><h1><a href="http://t.cn/E49YWj6">点击观看</a> 视频教程</h1> <h1><a href="http://ae85.cn/fuli/xiaoliang.html">点击领取</a> 支付宝红包</h1><h1><a href="http://ae85.cn/lxfs.html">关注公众号</a> 小良科技</h1><h2>by：iPhone 8、小良&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="http://ae85.cn/">http://ae85.cn/</a> </h2></span></body>`
         },
         layout: function (make) {
           make.top.equalTo($("wd").bottom).inset(10);
@@ -110,255 +195,135 @@ function cshyz() {
     ]
   });
 }
-var file = $file.read("key.txt");
-if (!file) {
-  cshyz();
-} else {
-  if (yzmm(file.string)) {
-    zjm();
-  } else {
-    cshyz();
-  }
-}
 
-function generateStr(a) {
-  var c = (function () {
-    for (var d = 0, f = new Array(256), g = 0; 256 != g; ++g) {
-      (d = g),
-        (d = 1 & d ? -306674912 ^ (d >>> 1) : d >>> 1),
-        (d = 1 & d ? -306674912 ^ (d >>> 1) : d >>> 1),
-        (d = 1 & d ? -306674912 ^ (d >>> 1) : d >>> 1),
-        (d = 1 & d ? -306674912 ^ (d >>> 1) : d >>> 1),
-        (d = 1 & d ? -306674912 ^ (d >>> 1) : d >>> 1),
-        (d = 1 & d ? -306674912 ^ (d >>> 1) : d >>> 1),
-        (d = 1 & d ? -306674912 ^ (d >>> 1) : d >>> 1),
-        (d = 1 & d ? -306674912 ^ (d >>> 1) : d >>> 1),
-        (f[g] = d);
-    }
-    return "undefined" != typeof Int32Array ? new Int32Array(f) : f;
-  })(),
-    b = function (g) {
-      for (var j, k, h = -1, f = 0, d = g.length; f < d;) {
-        (j = g.charCodeAt(f++)),
-          j < 128
-            ? (h = (h >>> 8) ^ c[255 & (h ^ j)])
-            : j < 2048
-              ? ((h = (h >>> 8) ^ c[255 & (h ^ (192 | ((j >> 6) & 31)))]),
-                (h = (h >>> 8) ^ c[255 & (h ^ (128 | (63 & j)))]))
-              : j >= 55296 && j < 57344
-                ? ((j = (1023 & j) + 64),
-                  (k = 1023 & g.charCodeAt(f++)),
-                  (h = (h >>> 8) ^ c[255 & (h ^ (240 | ((j >> 8) & 7)))]),
-                  (h = (h >>> 8) ^ c[255 & (h ^ (128 | ((j >> 2) & 63)))]),
-                  (h =
-                    (h >>> 8) ^
-                    c[255 & (h ^ (128 | ((k >> 6) & 15) | ((3 & j) << 4)))]),
-                  (h = (h >>> 8) ^ c[255 & (h ^ (128 | (63 & k)))]))
-                : ((h = (h >>> 8) ^ c[255 & (h ^ (224 | ((j >> 12) & 15)))]),
-                  (h = (h >>> 8) ^ c[255 & (h ^ (128 | ((j >> 6) & 63)))]),
-                  (h = (h >>> 8) ^ c[255 & (h ^ (128 | (63 & j)))]));
-      }
-      return h ^ -1;
-    };
-  return b(a) >>> 0;
-}
-
-function geturl(url) {
-  var c = Math.random()
-    .toString(10)
-    .substring(2);
-  var a = generateStr(url + "@" + c).toString(10);
-  var vid = $cache.get("vid");
-  var uhi = "LmlpaWvwYi5jb20";
-  var Ori = "http://" + vid + $text.base64Decode(uhi.replace(/vw/, "xh") + "=");
-  var lan = "aHR0cDovvw3NlcnZpY2UwvwmlpaWxhYi5jb20vdmlkZW8vd2Vivww";
-  $ui.loading(true);
-  $http.request({
-    method: "POST",
-    url: $text.base64Decode(lan.replace(/vw/g, "L") + "==") + vid,
-    header: {
-      "Content-Type": "application/x-www-form-urlencoded",
-      "Origin": Ori,
-      "Referer": Ori,
-      "Cookie": $cache.get("cookie"),
-      "User-Agent":
-        "Mozilla/5.0 (iPhone; CPU iPhone OS 9_3_3 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13G34 Safari/601.1"
-    },
-    body: {
-      "link": url,
-      "r": c,
-      "s": a
-    },
-    handler: function (resp) {
-      $ui.loading(false);
-      if (resp.data.retDesc == "成功") {
-        var json = resp.data.data;
-        var durl = json.video;
-        var lurl = durl.download;
-        if (lurl) {
-          $ui.menu({
-            items: lurl.map(function (item) {
-              return item.type;
-            }),
-            handler(title, idx) {
-              cut(lurl[idx].url);
-            }
-          });
-        } else {
-          cut(durl);
-        }
-      }
-    }
-  });
-}
-
-var html = `<head><meta charset="UTF-8"></head><body><span style="font-size:34px;"><br><h1>使用说明：</h1><h2>脚本已支持：皮皮虾、抖音、快手、火山、今日头条、西瓜视频、微博、秒拍、小咖秀、晃咖、微视、美拍、网易云音乐、陌陌、映客、小影 等平台的视频。<br><br>为了适配jsbox将取消自动提取功能</h2><br><h1><a href="http://t.cn/Evovggo">点击观看</a> 视频教程</h1> <h1><a href="http://ae85.cn/fuli/xiaoliang.html">点击领取</a> 支付宝红包</h1><h1><a href="http://ae85.cn/lxfs.html">关注公众号</a> VIP视频解析</h1><h2>by：iPhone 8、小良&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="http://ae85.cn/">http://ae85.cn/</a> </h2></span></body>`;
-
-function tmenu() {
-  $ui.menu({
-    items: all_type.map(function (item) {
-      return item.name;
-    }),
-    handler(title, idx) {
-      $cache.set("vid", all_type[idx].vid);
-      var text = $("bjk").text;
-      var url = $detector.link(text);
-      if (!url[0]) {
-        $ui.alert("请先输入链接");
-      } else {
-        $ui.toast("处理中，请稍后···")
-        $("webid").url = "http://" + all_type[idx].vid + ".iiilab.com/"
-      }
-    }
-  }); 
-}
+var timer;
 function zjm() {
   $ui.render({
     props: {
-      title: "短视频下载器 1.2"
+      title: "短视频下载 2.0"
     },
-    views: [
-      {
-        type: "input",
-        props: {
-          id: "bjk",
-          placeholder: "输入视频地址…"
-        },
-        layout: function (make) {
-          make.top.inset(10);
-          make.left.right.inset(10);
-          make.height.equalTo(35);
-        },
-        events: {
-          returned: function (sender) {
-            blur()
-            tmenu();
-          }
-        }
+    views: [{
+      type: "button",
+      props: {
+        title: "提取",
+        id: "bt1"
       },
-      {
-        type: "button",
-        props: {
-          title: "点击-这里选择你要提取的视频平台",
-          id: "bt1"
-        },
-        layout: function (make) {
-          make.top.equalTo($("bjk").bottom).inset(10);
-          make.right.left.inset(10);
-          make.height.equalTo(35);
-        },
-        events: {
-          tapped: function (sender) {
-            tmenu();
-          }
-        }
+      layout: function (make) {
+        make.top.inset(20);
+        make.right.inset(10);
+        make.width.equalTo(80)
+        make.height.equalTo(35);
       },
-      {
-        type: "web",
-        props: {
-          id: "webxia",
-          html: `<head><meta charset="UTF-8"></head><body><span style="font-size:34px;"><br><h1>使用说明：</h1><h2>脚本已支持：皮皮虾、抖音、快手、火山、今日头条、西瓜视频、微博、秒拍、小咖秀、晃咖、微视、美拍、网易云音乐、陌陌、映客、小影 等平台的视频。<br><br>为了适配jsbox将取消自动提取功能</h2><br><h1><a href="http://t.cn/Evovggo">点击观看</a> 视频教程</h1> <h1><a href="http://ae85.cn/fuli/xiaoliang.html">点击领取</a> 支付宝红包</h1><h1><a href="http://ae85.cn/lxfs.html">关注公众号</a> VIP视频解析</h1><h2>by：iPhone 8、小良&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="http://ae85.cn/">http://ae85.cn/</a> </h2></span></body>`
-        },
-        layout: function (make) {
-          make.top.equalTo($("bt1").bottom).inset(10);
-          make.bottom.inset(0);
-          make.left.right.inset(10);
-        }
-      },
-      {
-        type: "button",
-        props: {
-          id: "bt3",
-          radius: 25,
-          src: "http://ae85.cn/wf/hb.jpg",
-          font: $font(20)
-        },
-        layout: function (make) {
-          make.right.inset(15);
-          make.top.equalTo($("bt1").bottom).inset(20);
-          make.height.width.equalTo(50);
-        },
-        events: {
-          tapped: function (sender) {
-            $app.openURL(
-              "alipays://platformapi/startapp?saId=10000007&clientVersion=3.7.0.0718&qrcode=https%3A%2F%2Fqr.alipay.com%2Fc1x08786tiy5qigyhnjd2db%3F_s%3Dweb-other"
-            );
-          },
-          longPressed: function (sender) {
-            $("webxia").html = html;
-            $("bt4").alpha = 0;
-          }
-        }
-      },
-      {
-        type: "button",
-        props: {
-          id: "bt4",
-          src: "http://ae85.cn/jsbox/img/xiazai.png",
-          font: $font(20),
-          alpha: 0
-        },
-        layout: function (make) {
-          make.right.inset(15);
-          make.top.equalTo($("bt3").bottom).offset(20);
-          make.height.width.equalTo(50);
-        },
-        events: {
-          tapped: function (sender) {
-            download($cache.get("url"));
-          },
-          longPressed: function (sender) {
-            var url = $cache.get("url");
-            $clipboard.text = url;
-            $ui.alert("已复制：\n" + url);
-          }
-        }
-      },
-      {
-        type: "web",
-        props: {
-          id: "webid",
-          url: "http://kuaishou.iiilab.com/",
-          script: function () {
-            var Html = window.parent.document.cookie;
-            $notify("customEvent", Html);
-          }
-        },
-        layout: function (make, view) {
-          make.left.bottom.inset(0);
-          make.width.height.equalTo(0);
-        },
-
-        events: {
-          customEvent: function (object) {
-            $cache.set("cookie", object);
-            var text = $("bjk").text
-            var url = $detector.link(text);
-            geturl(url[0]);
-          },
+      events: {
+        tapped: function (sender) {
+          $('vediobtn').hidden = true;
+          $('webxia').hidden = false;
+          tmenu($('bjk').text);
         }
       }
-    ]
+    }, {
+      type: "input",
+      props: {
+        id: "bjk",
+        placeholder: "输入视频地址…"
+      },
+      layout: function (make) {
+
+        make.top.inset(20);
+        make.left.inset(10);
+        make.right.equalTo($("bt1").left).inset(10);
+        make.height.equalTo(35);
+      },
+      events: {
+        returned: function (sender) {
+          $("bjk").blur();
+          tmenu($('bjk').text);
+        }
+      }
+    }, {
+      type: "web",
+      props: {
+        id: "webxia",
+        html: `<head><meta charset="UTF-8"></head><body><span style="font-size:34px;"><br><h1>使用说明：</h1><h2>脚本已支持：皮皮虾、抖音、快手、火山、今日头条、西瓜视频、微博、秒拍、小咖秀、晃咖、微视、美拍、网易云音乐、陌陌、映客、小影 等平台的视频。<br><br>2.0适配pin有自动提取功能</h2><br><h1><a href="http://t.cn/E49YWj6">点击观看</a> 视频教程</h1> <h1><a href="http://ae85.cn/fuli/xiaoliang.html">点击领取</a> 支付宝红包</h1><h1><a href="http://ae85.cn/lxfs.html">关注公众号</a> 小良科技</h1><h2>by：iPhone 8、小良&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="http://ae85.cn/">http://ae85.cn/</a> </h2></span></body>`
+      },
+      layout: function (make) {
+        make.top.equalTo($("bt1").bottom).inset(15);
+        make.left.right.inset(10);
+        make.bottom.inset(0)
+      }
+    },
+    {
+      type: "web",
+      props: {
+        id: "web",
+      },
+      layout: function (make, view) {
+        make.top.equalTo(0)
+        make.right.left.inset(0)
+        make.height.equalTo(1)
+      }
+    }, {
+      type: 'view',
+      props: {
+        hidden: true,
+        id: 'vediobtn'
+      },
+      layout: function (make, view) {
+        make.left.bottom.right.inset(10);
+        make.top.equalTo($("bt1").bottom).inset(20);
+      },
+      views: [{
+        type: 'video',
+        props: {
+          id: 'videobof',
+          radius: 7,
+          bgcolor: $color('#eee')
+        },
+        layout: function (make, view) {
+          make.left.right.inset(10);
+          make.top.equalTo(20);
+          make.height.equalTo(250);
+        }
+      }, {
+        type: 'button',
+        props: {
+          title: '下载视频',
+          id: 'dBtn',
+          bgcolor: $color('#409eff')
+        },
+        layout: function (make, view) {
+          make.top.equalTo($('videobof').bottom).inset(30)
+          make.left.inset(20);
+          make.height.equalTo(50);
+          make.width.equalTo(130)
+        },
+        events: {
+          tapped: function (sender) {
+            download($cache.get("url"))
+          }
+        }
+      },
+      {
+        type: 'button',
+        props: {
+          title: '复制链接',
+          id: 'fBtn',
+          bgcolor: $color('#909399')
+        },
+        layout: function (make, view) {
+          make.top.equalTo($('videobof').bottom).inset(30)
+          make.right.inset(20);
+          make.height.equalTo(50);
+          make.width.equalTo(130)
+        },
+        events: {
+          tapped: function (sender) {
+            $clipboard.text = $cache.get("url");
+            $ui.toast('复制成功!');
+          }
+        }
+      }]
+    }]
   });
 }
 function yzmm(t) {
@@ -369,13 +334,20 @@ function yzmm(t) {
     return 0;
   }
 }
-function csh() {
-  $("bt4").alpha = 0;
-  var durl = $clipboard.link;
-  if (durl) {
-    $("bjk").text = durl;
-   // tmenu();
-  }
+function zhur() {
+  var webView = $("web")
+  webView.eval({
+    script: `var URL = document.getElementsByClassName("btn btn-success")[0].href; window.name = URL`,
+    handler: function (result, error) {
+      if (result.indexOf('http') !== -1) {
+        $cache.set("url", result);
+        $('vediobtn').hidden = false;
+        $('webxia').hidden = true;
+        $('videobof').src = result;
+        timer.invalidate()
+      }
+    }
+  })
 }
 function download(url) {
   $ui.toast("正在下载中 ...");
@@ -392,10 +364,21 @@ function download(url) {
     }
   });
 }
-function cut(url) {
-  $cache.set("url", url);
-  $("webxia").url = url;
-  $("bt4").alpha = 1;
-  download(url)
+
+function tmenu(text) {
+  var url = $detector.link(text);
+  if (!url[0]) {
+    $ui.alert("请先输入链接");
+  } else {
+    $ui.toast("处理中，请稍后···")
+    var turl = $cache.get("info").turl
+    $('web').url = $text.base64Decode(turl) + url;
+    timer = $timer.schedule({
+      interval: 3,
+      handler: function () {
+        zhur()
+      }
+    });
+  }
 }
-csh()
+
