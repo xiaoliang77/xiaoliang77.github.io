e7c85d4fd81ef371439f00fc70436660b95d1472
liangziai77 <32543205+liangziai77@users.noreply.github.com>
2019-01-09 10:43:44 +0800

    更新

--- pin/jb/sszb.js
+++ pin/jb/sszb.js
@@ -1,154 +1,270 @@
 /*
-2018年12月18日 修复更新
-更新：更换直播源
-原先的资源已经停止更新了
+2019年1月8日 修复更新
+更新：修复部分不能播放问题
+
 by：iPhone 8、小良
 http://ae85.cn/
 
 */
 
-$ui.render({
-  props: {
-    title: "赛事直播 1.3"
-  },
-  views: [{
-    type: "label",
-    props: {
-      id: "rq",
-      text: "加载中……",
-      textColor: $color("#008062"),
-      align: $align.center
-    },
-    layout(make, view) {
-      make.top.inset(5)
-      make.right.left.inset(0)
-      make.height.equalTo(30)
+$ui.loading(true);
+$http.get({
+    url: $text.base64Decode("aHR0cHM6Ly9naXRlZS5jb20veWFvMDcvdXBkYXRlX2RldmljZS9yYXcvbWFzdGVyLw==") + "saishi.json",
+    handler: function (resp) {
+        $ui.loading(false);
+        if (resp.data.bb != "1.4") {
+            $ui.alert({
+                title: "温馨提示：",
+                message: resp.data.gxsm,
+                actions: [
+                    {
+                        title: "访问官网",
+                        handler: function () {
+                            $app.openURL(resp.data.gw);
+                        }
+                    },
+                    {
+                        title: "关注公众号",
+                        handler: function () {
+                            $ui.alert({
+                                title: "温馨提示",
+                                message: "跳转微信会自动复制公众号ID\n请跳转到微信-搜索-公用号-粘贴-关注",
+                                actions: [{
+                                    title: "跳转微信",
+                                    handler: function () {
+                                        $clipboard.text = "ae85-cn"
+                                        $app.openURL("weixin://")
+                                    }
+                                }, {
+                                    title: "取消"
+                                }]
+                            })
+                        }
+                    }
+                ]
+            });
+        } else {
+            $cache.set("info", resp.data);
+            tcgg(resp.data.gg)
+            $ui.render({
+                props: {
+                    title: "赛事直播1.4"
+                },
+                views: [{
+                    type: "list",
+                    props: {
+                        rowHeight: 80,
+                        template: [{
+                            type: "image",
+                            props: {
+                                id: "lt1",
+                                radius: 7,
+                                bgcolor: $color("white")
+                            },
+                            layout: function (make, view) {
+                                make.top.inset(10)
+                                make.left.inset(30)
+                                make.size.equalTo(40, 40)
+
+                            }
+                        }, {
+                            type: "image",
+                            props: {
+                                id: "lt2",
+                                radius: 7,
+                                bgcolor: $color("white")
+                            },
+                            layout: function (make, view) {
+                                make.top.inset(10)
+                                make.right.inset(30)
+                                make.size.equalTo(40, 40)
+                            }
+                        },
+                        {
+                            type: "label",
+                            props: {
+                                id: "sais",
+                                font: $font("bold", 16),
+                                align: $align.center
+                            },
+                            layout: function (make, view) {
+                                make.centerX.equalTo(view.center)
+                                make.top.inset(10)
+                                make.width.equalTo(200)
+
+                            }
+                        }, {
+                            type: "label",
+                            props: {
+                                id: "rq",
+                                font: $font(15),
+                                textColor: $color("red"),
+                                align: $align.center
+                            },
+                            layout: function (make, view) {
+                                make.centerX.equalTo(view.center)
+                                make.top.equalTo($("sais").bottom).inset(5)
+
+                            }
+                        }, {
+                            type: "label",
+                            props: {
+                                id: "zt",
+                                font: $font(15),
+                                textColor: $color("#c4c4c4c"),
+                                align: $align.center
+                            },
+                            layout: function (make, view) {
+                                make.centerX.equalTo(view.center)
+                                make.top.equalTo($("rq").bottom).inset(5)
+
+                            }
+                        },
+                        {
+                            type: "label",
+                            props: {
+                                id: "zd1",
+                                font: $font(15),
+                                textColor: $color("gray"),
+                                align: $align.center
+                            },
+                            layout: function (make) {
+                                make.left.equalTo(0)
+                                make.top.equalTo($("lt1").bottom).inset(5)
+                                make.width.equalTo(100)
+                            }
+                        },
+                        {
+                            type: "label",
+                            props: {
+                                id: "zd2",
+                                font: $font(15),
+                                textColor: $color("gray"),
+                                align: $align.center
+                            },
+                            layout: function (make) {
+                                make.right.equalTo(0)
+                                make.top.equalTo($("lt2").bottom).inset(5)
+                                make.width.equalTo(100)
+                            }
+                        }
+
+                        ]
+                    },
+                    layout: $layout.fill,
+                    events: {
+                        didSelect: function (sender, indexPath, data) {
+                            getzb(data.id, data.sais.text)
+                        }
+                    }
+                }]
+
+            })
+            getlb()
+        }
     }
-  },{
-    type: "list",
-    props: {
-      rowHeight: 50,
-      template: [
-        {
-          type: "label",
-          props: {
-            id: "mc",
-            font: $font("bold", 17),
-          },
-          layout: $layout.fill
+});
+function tcgg(gg) {
+    if ($file.exists("gg.txt")) {
+        var file = $file.read("gg.txt").string;
+        if (file != gg) {
+            xrwj(gg);
+        }
+    } else {
+        xrwj(gg);
+    }
+}
+function xrwj(nr) {
+    $ui.alert(nr);
+    $file.write({
+        data: $data({ string: nr }),
+        path: "gg.txt"
+    });
+}
+function play(url, mc) {
+    $ui.push({
+        props: {
+            title: mc
         },
-      ]
-    },
-    layout(make, view) {
-      make.top.equalTo($("rq").bottom).inset(5)
-      make.right.left.bottom.inset(0)
-    },
-    events: {
-      didSelect: function (sender, indexPath, data) {
-        setxq(data.menu[0].url, data.mc.text, data.menu)
-      }
-    }
-  }]
-
-})
-
-var urlt = "http://m.360zhibo.com"
+        views: [{
+            type: "web",
+            props: {
+                id: "bo",
+                url: url,
+            },
+            layout: $layout.fill
+        }]
+    })
+}
 
 function getlb() {
-  $ui.loading(true)
-  $http.get({
-    url: urlt,
-    handler: function (resp) {
-      $ui.loading(false)
-      var html = resp.data.replace(/\n|\s|\r/g, "")
-      var li = html.split('<divclass="col_02">')[2]
-      var t1 = li.match(/<liondblclick=.*?<\/li>/g)
-      var biao = li.match(/<divclass=\"title1\">(\S*?)<\/div>/)[1]
-      var data = []
-      for (i in t1) {
-        var text = t1[i].match(/<spanclass=(\S*?)<\/div>/)[1]
-        text = text.replace(/\"time\">|\"imp\">|<\/span>|<span>|<ahref=\"\/cat\/cba\/\">|<ahref=\"\/cat\/lanqiu\/\">|<\/a>/g, " ")
-        var href = t1[i].match(/<ahref=\".*?<\/a>/g)
-        var menu = []
-        for (n in href) {
-          var name = href[n].match(/\">(\S*?)<\/a>/)[1]
-          var url = href[n].match(/href=\"(\S*?)\"/)[1]
-          menu.push({
-            name: name.replace(/\(.*?\)/g, ''),
-            url: url
-          })
-        }
-        data.push({
-          mc: { text: text },
-          menu: menu
-        })
-      }
-      $("rq").text = biao
-      $("list").data = data
-    }
-  })
-}
-getlb()
-
-function setxq(url, pm, meun) {
-  $ui.push({
-    props: {
-      title: pm
-    },
-    views: [{
-      type: "menu",
-      props: {
-        id: "meun",
-        items: meun.map(function (item) {
-          return item.name
-        }),
-      },
-      layout: function (make) {
-        make.left.top.right.equalTo(0)
-        make.height.equalTo(50)
-
-      },
-      events: {
-        changed: function (sender) {
-          getpy(meun[sender.index].url)
-        }
-      }
-    }, {
-      type: "web",
-      props: {
-        id: "bof"
-      },
-      layout: function (make, view) {
-        make.right.left.bottom.inset(0)
-        make.top.equalTo($("meun").bottom).inset(5)
-      }
-    },]
-  })
-  getpy(url)
-}
-
-function getpy(url) {
-  if (url.indexOf('http') == -1) {
     $ui.loading(true)
-    $http.request({
-      method: "GET",
-      url: urlt + url,
-      header: {
-        "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 11_3_3 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13G34 Safari/601.1",
-        "Referer": urlt
-      },
-      handler: function (resp) {
-        $ui.loading(false)
-        var html = resp.data.replace(/\n|\s|\r/g, "")
-        if (html.indexOf('dw(') == -1) {
-          alert("未找到该直播源地址")
-        } else {
-          var u = html.match(/dw\(\'(\S*?)\'\)/)[1]
-          $("bof").url = $detector.link(u)[0];
+    $http.get({
+        url: $text.base64Decode($cache.get("info").turl) + "/Api/V1_5/Zhibo/anchor?v=ios_1.5.6",
+        header: {
+            "User-Agent": "PlayBall_IOS/1.0 (iPhone; iOS 12.0; Scale/3.00)"
+        },
+        handler: function (resp) {
+            $ui.loading(false)
+            var html = resp.data.data.zhibo_data
+            var data = []
+            for (a in html) {
+                var t1 = html[a].data
+                for (i in t1) {
+                    var sjian = format(t1[i].start_time)
+                    data.push({
+                        lt1: {
+                            src: t1[i].home_logo
+                        },
+                        lt2: {
+                            src: t1[i].visit_logo
+                        },
+                        sais: { text: t1[i].name },
+                        rq: { text: sjian },
+                        zt: { text: t1[i].status_ch },
+                        zd1: { text: t1[i].home_team },
+                        zd2: { text: t1[i].visit_team },
+                        id: t1[i].zhibo_id
+                    })
+                }
+            }
+            $("list").data = data
+
         }
-      }
     })
-  } else {
-    $("bof").url = $detector.link(url)[0];
-  }
+}
+function format(sjc) {
+    var shi = parseInt(sjc) * 1000
+    var time = new Date(shi);
+    var m = time.getMonth() + 1;
+    var d = time.getDate();
+    var h = time.getHours();
+    var mm = time.getMinutes();
+    return m + "月" + d + "日" + " " + h + ":" + mm
+}
+
+function getzb(id, mc) {
+    $ui.loading(true)
+    $http.get({
+        url: $text.base64Decode($cache.get("info").turl) + "/Api/V1_5/Zhibo/zhiboDetail/zhibo_id/"+id+"?v=ios_1.5.6",
+        header: {
+            "User-Agent": "PlayBall_IOS/1.0 (iPhone; iOS 12.0; Scale/3.00)"
+        },
+        handler: function (resp) {
+            $ui.loading(false)
+            var html = resp.data.data.zhibo_info.url
+            
+            if (html) {
+                $ui.menu({
+                    items: html.map(add => { return add.name }),
+                    handler: function (title, idx) {
+
+                        play(html[idx].url, mc)
+                    }
+                });
+            } else {
+                alert("暂无该赛事直播信息")
+            }
+        }
+    })
 }
