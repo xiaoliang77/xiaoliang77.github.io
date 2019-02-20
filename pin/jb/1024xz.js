aa263c5ddacf65f3f81559b977b73041422f34a5
liangziai77 <32543205+liangziai77@users.noreply.github.com>
2019-02-20 10:32:46 +0800

    Update 1024xz.js

--- pin/jb/1024xz.js
+++ pin/jb/1024xz.js
@@ -8,7 +8,7 @@ https://ae85.cn/
 
 $cache.set("id", "5")
 $cache.set("pg", 1)
-var urlt = "http://w2.hj97zhx837.pw/"
+var urlt = "http://w1.97xzjpzz.info/"
 var data = [{ "name": "亚洲無碼", "id": "5" }, { "name": "日本騎兵", "id": "22" }, { "name": "歐美新片", "id": "7" }, { "name": " 三級寫真", "id": "18" },]
 
 $ui.render({
