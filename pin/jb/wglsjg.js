9d7dfdca68f8dff0eda1341b90831f52156c2c87
liangziai77 <84088289@qq.com>
2019-04-10 20:34:48 +0800

    gx

--- pin/jb/wglsjg.js
+++ pin/jb/wglsjg.js
@@ -1,15 +1,17 @@
 /*
-网购历史价格查询 1.1
-新增：自动查询商品隐藏优惠券。
+2019年4月10日 更新
+
+网购历史价格查询 1.2
+功能：自动查询商品隐藏优惠券。
 支持：淘宝、京东、舒宁、亚马逊、等各大网商商品历史价格查询。
 
 by：iPhone 8、小良
-https://ae85.cn/
+http://ae85.cn/
 */
 var ycq;
 $ui.render({
     props: {
-        title: "网购历史价格查询 1.1",
+        title: "网购历史价格查询 1.2",
 
     },
     views: [{
@@ -19,7 +21,7 @@ $ui.render({
             id: "bt1"
         },
         layout: function (make) {
-            make.top.inset(20);
+            make.top.inset(10);
             make.right.inset(10);
             make.width.equalTo(80)
             make.height.equalTo(35);
@@ -37,7 +39,7 @@ $ui.render({
             placeholder: "输入商品地址…"
         },
         layout: function (make) {
-            make.top.inset(20);
+            make.top.inset(10);
             make.left.inset(10);
             make.right.equalTo($("bt1").left).inset(10);
             make.height.equalTo(35);
@@ -56,137 +58,32 @@ $ui.render({
         },
         layout: function (make, view) {
             make.left.bottom.right.inset(0);
-            make.top.equalTo($("bjk").bottom).inset(20);
+            make.top.equalTo($("bjk").bottom).inset(10);
         },
         views: [{
-            type: "image",
-            props: {
-                id: "lt",
-                bgcolor: $color("white")
-            },
-            layout: function (make, view) {
-                make.left.top.inset(10);
-                make.size.equalTo($size(100, 100));
-            }
-        }, {
-            type: "label",
-            props: {
-                id: "b1",
-                lines: 3,
-                font: $font(15),
-            },
-            layout(make, view) {
-                make.top.inset(10)
-                make.right.inset(0)
-                make.left.equalTo($("lt").right).inset(10)
-
-            }
-        }, {
-            type: "label",
-            props: {
-                id: "b2",
-                font: $font(13),
-                textColor: $color("#c4c4c4"),
-            },
-            layout(make, view) {
-                make.bottom.equalTo($("lt").bottom)
-                make.right.inset(0)
-                make.left.equalTo($("b1").left)
-            }
-        }, {
-            type: "label",
-            props: {
-                id: "b3",
-                font: $font("bold", 17),
-                textColor: $color("#F00"),
-            },
-            layout(make, view) {
-                make.bottom.equalTo($("b2").top).inset(3)
-                make.right.inset(0)
-                make.left.equalTo($("b1").left)
-            }
-        }, {
-            type: 'view',
-            props: {
-                bgcolor: $color("#f2f2f2"),
-                id: 'fgf'
-            },
-            layout: function (make, view) {
-                make.left.right.inset(0);
-                make.top.equalTo($("lt").bottom).inset(20);
-                make.height.equalTo(20)
-
-            }
-        }, {
-            type: "label",
-            props: {
-                id: "jgzs",
-                text: "价格走势",
-                font: $font("bold", 17),
-            },
-            layout(make, view) {
-                make.top.equalTo($("fgf").bottom).inset(15)
-                make.left.inset(10)
-                make.width.equalTo(100)
-            }
-        }, {
-            type: "label",
-            props: {
-                id: "zdj",
-                font: $font(16),
-                textColor: $color("#F00"),
-            },
-            layout(make, view) {
-                make.top.equalTo($("jgzs").top)
-                make.right.inset(10)
-            }
-        }, {
-            type: "label",
-            props: {
-                text: "历史最低价: ",
-                font: $font(16),
-            },
-            layout(make, view) {
-                make.top.equalTo($("jgzs").top)
-                make.right.equalTo($("zdj").left)
-            }
-        }, {
-            type: 'view',
-            props: {
-                bgcolor: $color("#f2f2f2"),
-                id: 'fgf1'
-            },
-            layout: function (make, view) {
-                make.left.right.inset(0);
-                make.top.equalTo($("jgzs").bottom).inset(15);
-                make.height.equalTo(1)
-
-            }
-        }, {
             type: "web",
             props: {
                 id: "web",
                 scrollEnabled: 0,
                 showsProgress: 0,
                 script: function () {
-                    document.getElementsByClassName("header-bar border-bottom-img")[0].style.display = "none";
+                    document.getElementsByClassName("g")[0].style.display = "none";
                     document.getElementsByClassName("app-promotion-bar")[0].style.display = "none";
-                    document.getElementsByClassName("t")[0].style.display = "none";
-                    document.getElementsByClassName("input")[0].style.display = "none";
-                    document.getElementsByClassName("btn")[0].style.display = "none";
-                    document.getElementsByClassName("spinfo")[0].style.display = "none";
+                    document.getElementsByClassName("letTry")[0].style.display = "none";
+                    document.getElementsByClassName("trend_page")[0].style.display = "none";
+                    document.getElementsByClassName("note")[0].style.display = "none";
                     $notify("share")
                 }
             },
             layout: function (make, view) {
-                make.top.equalTo($("fgf1").bottom)
+                make.top.equalTo(0)
                 make.right.left.inset(0)
-                make.height.equalTo(250)
+                make.bottom.inset(100)
             },
-
             events: {
                 share: function (obj) {
-                    $('web').hidden = false;
+                    $('xxjgjm').hidden = false;
+                    $('jzdh').hidden = true;
                 },
             }
         }, {
@@ -207,7 +104,7 @@ $ui.render({
                     bgcolor: $color("#f40"),
                 },
                 layout: function (make) {
-                    make.top.inset(20);
+                    make.top.inset(0);
                     make.left.right.inset(20);
                     make.height.equalTo(35);
                 },
@@ -234,7 +131,6 @@ $ui.render({
 })
 
 function tmenu(text) {
-    $('web').hidden = true;
     $('xxjgjm').hidden = true;
     $('jzdh').hidden = false;
     $('quanBt').title = "直接购买"
@@ -243,38 +139,11 @@ function tmenu(text) {
         $ui.alert("请先输入链接");
     } else {
         $ui.toast("处理中，请稍后···")
-        $('web').url = "http://tool.manmanbuy.com/m/history.aspx?url=" + $text.URLEncode(url);
-        getlsjg(url)
+        $('web').url = "http://tool.manmanbuy.com/m/disSitePro.aspx?c_from=mmbapp&url=" + $text.URLEncode(url);
+        GetUrlParam(url[0])
     }
 }
 
-function getlsjg(text) {
-    $ui.loading(true)
-    $http.get({
-        url: "https://apapia.manmanbuy.com/ChromeWidgetServices/WidgetServices.ashx?p_url=" + $text.URLEncode(text) + "&methodName=getBiJiaInfo_wxsmall&jsoncallback=&jgzspic=no",
-        handler: function (resp) {
-            $ui.loading(false)
-            if (resp.data.ok == 1) {
-                var add = resp.data.single
-                $('b1').text = add.title
-                $('lt').src = add.bigpic
-                $('b2').text = add.zk_scname
-                $('b3').text = "￥" + add.spmoney
-                $('zdj').text = "￥" + add.lowerPrice
-                if (add.zk_scname == "淘宝天猫") {
-                    ycq = add.url.replace(/http:\/\/|https:\/\//, "taobao://")
-                } else {
-                    ycq = add.url
-                }
-                $('xxjgjm').hidden = false;
-                $('jzdh').hidden = true;
-                getzk(add.id)
-            } else {
-                alert(resp.data.msj)
-            }
-        }
-    })
-}
 
 function getzk(id) {
     $ui.loading(true)
@@ -289,7 +158,7 @@ function getzk(id) {
                 ycq = quan.quan_url.replace(/http:\/\/|https:\/\//, "taobao://")
                 $('quanBt').title = "领取隐藏优惠券：" + quan.quan_remark
             } else {
-                $ui.toast("未找到该商品隐藏优惠券")
+                $ui.error("未找到该商品隐藏优惠券")
                 $('quanBt').title = "未找到隐藏优惠券 - 直接购买"
             }
         }
@@ -314,3 +183,21 @@ if (url) {
         }]
     })
 }
+
+function GetUrlParam(url) {
+    var arrObj = url.split("?");
+    if (arrObj.length > 1) {
+        var arrPara = arrObj[1].split("&");
+        var arr;
+        for (var i = 0; i < arrPara.length; i++) {
+            arr = arrPara[i].split("=");
+            if (arr != null && arr[0] == "id") {
+                getzk(arr[1])
+            }
+        }
+    }
+    else {
+        $ui.error("未找到该商品id信息");
+    }
+}
+
