0700d69a63b9682eb8b8dc2034069fd6287a4480
liangziai77 <84088289@qq.com>
2019-04-24 14:44:16 +0800

    gx

--- xcss/gz.js
+++ xcss/gz.js
@@ -11,7 +11,7 @@ window.open(url)
 var url="workflow://import-workflow/?url=https%3A%2F%2Fgitee.com%2Fyao07%2Fupdate_device%2Fraw%2Fmaster%2F%25E8%25A7%2584%25E5%2588%2599%2FYouTube%25E8%25A7%2586%25E9%25A2%2591%25E4%25B8%258B%25E8%25BD%25BD&name=YouTube%E8%A7%86%E9%A2%91%E4%B8%8B%E8%BD%BD"
 window.open(url)
 }else if(id=="1"){
-	 var url="workflow://import-workflow/?url=https%3A%2F%2Fgitee.com%2Fyao07%2Fupdate_device%2Fraw%2Fmaster%2Fgxq&name=%E5%B0%8F%E8%89%AF%20-%20%E6%9B%B4%E6%96%B0%E5%99%A8%203.4"
+	 var url="workflow://import-workflow/?url=https%3A%2F%2Fgitee.com%2Fyao07%2Fupdate_device%2Fraw%2Fmaster%2F%25E8%25A7%2584%25E5%2588%2599%2F%25E5%25B0%258F%25E8%2589%25AF%2520-%2520%25E6%259B%25B4%25E6%2596%25B0%25E5%2599%25A8%25203.5.wflow&name=%E5%B0%8F%E8%89%AF%20-%20%E6%9B%B4%E6%96%B0%E5%99%A8%203.5"
     window.open(url)
   }else{
 	var url="https://workflow.is/workflows/"+id
