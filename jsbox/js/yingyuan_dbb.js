5dd5328453033fe4cb0ce84457584220f69c3219
liangzaiai77 <84088289@qq.com>
2021-02-13 01:20:41 +0800

    更新

---
+++ jsbox/js/yingyuan_dbb.js
@@ -0,0 +1,175 @@
+/*
+2021年2月12日 更新
+祝大家新年快乐,新年第一更！
+
+by：iPhone 8、小良
+http://ae85.cn/
+*/
+const gzgzh = {
+  title: "关注公众号",
+  handler: function () {
+    $ui.alert({
+      title: "温馨提示",
+      message: "跳转微信会自动复制公众号ID\n请跳转到微信-搜索-公用号-粘贴-关注",
+      actions: [
+        {
+          title: "跳转微信",
+          handler: function () {
+            $clipboard.text = "ae85-cn";
+            $app.openURL("weixin://");
+          }
+        },
+        {
+          title: "取消"
+        }
+      ]
+    });
+  }
+};
+
+const base64 =
+  "aHR0cHM6Ly8naXRlZS5jb20veWFvMDcvdXBkYXRlX2RldmljZS8yYXcvbWFzdGVyL2RvdXlpbi5qc28u";
+$ui.loading(true);
+$http.get({
+  url: $text.base64Decode(base64.replace(/8/g, "9")),
+  header: {
+    "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Mobile/15E148 Safari/604.1"
+  },
+  handler: function (resp) {
+    $ui.loading(false);
+    if (resp.response.statusCode == "200") {
+      var info = resp.data.guonei;
+      if (info.bb != "1.3") {
+        $ui.alert({
+          title: "温馨提示",
+          message: info.gxsm,
+          actions: [
+            {
+              title: "访问官网",
+              handler: function () {
+                $app.openURL(info.gw);
+              }
+            },
+            gzgzh
+          ]
+        });
+      } else {
+        $cache.set("info", info);
+        Pgetdata();
+      }
+    } else {
+      $ui.alert("加载失败");
+    }
+  }
+});
+
+$cache.set("pg", 1);
+$ui.render({
+  props: {
+    title: "抖yin国内版 1.3",
+  },
+  views: [
+    {
+      type: "matrix",
+      props: {
+        id: "PVideo",
+        itemHeight: 200,
+        columns: 3,
+        spacing: 0,
+        bgcolor: $color("black"),
+        template: [
+          {
+            type: "image",
+            props: {
+              id: "img",
+            },
+            layout: function (make, view) {
+              make.centerX.equalTo(view.super);
+              make.top.bottom.right.left.inset(1);
+            }
+          }
+        ]
+      },
+      layout: function (make) {
+        make.top.bottom.left.right.inset(0);
+      },
+      events: {
+        didSelect: function (sender, indexPath, data) {
+          getvideo(data.url)
+        },
+        didReachBottom: function (sender) {
+          sender.endFetchingMore();
+          var page = $cache.get("pg") + 1;
+          $cache.set("pg", page);
+          Pgetdata();
+        }
+      }
+    }
+  ]
+});
+
+
+function Pgetdata() {
+  var info = $cache.get("info");
+  var pg = $cache.get("pg");
+  $http.get({
+    url: info.turl + "/video?page=" + pg,
+    header: {
+      "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Mobile/15E148 Safari/604.1"
+    },
+    handler: resp => {
+      var text = resp.data.replace(/\n|\r/g, "");
+      var shu = text.match(/<div class=\"box-shadow card\">(\s*.*?\S*)<\/div>/g)
+      var data = $("PVideo").data;
+      for (i in shu) {
+        var li = shu[i];
+        var img = li.match(/src=\"(\s*.*?\S*)\"/)[1]
+        img = $text.URLEncode(img);
+        img = img.replace(/%2F/g, "/");
+        img = img.replace(/%3A/g, ":");
+        var url = li.match(/href=\"(\S*?)\"/)[1]
+        data.push({
+          img: { src: img },
+          url: url
+        });
+      }
+      $("PVideo").data = data;
+      $("PVideo").endRefreshing();
+    }
+  });
+}
+function getvideo(url) {
+  var info = $cache.get("info");
+  $http.get({
+    url: info.turl + url,
+    header: {
+      "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Mobile/15E148 Safari/604.1"
+    },
+    handler: resp => {
+      var text = resp.data.replace(/\n|\r/g, "");
+      var video = text.match(/<source src=\"(\s*.*?\S*)\"/)[1]
+      var playurl = $text.URLEncode(video);
+      playurl = playurl.replace(/%2F/g, "/");
+      playurl = playurl.replace(/%3A/g, ":");
+      playurl = playurl.replace(/%3F/g, "?");
+      playurl = playurl.replace(/%3D/g, "=");
+      playvideo(playurl);
+    }
+  });
+}
+
+function playvideo(playurl) {
+  $ui.push({
+    props: {
+      title: "抖yin国内版 1.3"
+    },
+    views: [{
+      type: "web",
+      props: {
+        url: playurl,
+      },
+      layout: $layout.fill
+    }]
+  })
+
+}
