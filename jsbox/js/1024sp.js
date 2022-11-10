1bd81e70fb5d9712cf84df6ddd97d5cafca317ba
小良 <84088289@qq.com>
2022-11-10 20:54:09 +0800

    更新1024

--- jsbox/js/1024sp.js
+++ jsbox/js/1024sp.js
@@ -1,5 +1,6 @@
 /*
 2022年11月10日 更新
+新增自动更新功能
 脚本仅供代码学习，请勿分享。非法传播照成法律问题与作者无关。
 
 by：iPhone 8、小良
@@ -10,11 +11,12 @@ https://iphone8.vip/
 $cache.set("id", "21")
 $cache.set("pg", 1)
 var urlt = $text.base64Decode("aHR0cHM6Ly9ndWFuZ3hpYmlhb21laTIzOC50b3A=");
+var js_name = "1024视频"
 var data = [{ "name": "国产", "id": "21" }, { "name": "日本", "id": "22" }, { "name": "欧美", "id": "3" },{ "name": "三级", "id": "20" }, { "name": "动漫", "id": "14" }]
 
 $ui.render({
     props: {
-        title: "1024视频"
+        title: js_name
     },
     views: [{
         type: "menu",
@@ -116,6 +118,61 @@ function geting(id, mc) {
                     },
                     layout: $layout.fill
                 }]
+            })
+        }
+    })
+}
+
+
+async function get_updata() {
+    const resp = await $http.get($text.base64Decode("aHR0cHM6Ly9pcGhvbmU4LnZpcC9jb25maWcvMTAyNC5qc29u"));
+    if(resp.response.statusCode === 200){
+        if (resp.data.vdieo.version != "2.0") {
+            $ui.alert({
+                title: "发现新版本 - " + resp.data.vdieo.version,
+                message: resp.data.vdieo.upexplain,
+                actions: [
+                    {
+                        title: "立即更新",
+                        handler: function () {
+                            download(resp.data.vdieo.updata)
+                        }
+                    }, {
+                        title: "取消"
+                    }
+                ]
+
+            });
+            
+        }
+    }
+}
+get_updata()
+
+function download(url) {
+    $ui.toast("正在安装中 ...");
+    $http.download({
+        url: url,
+        handler: function (resp) {
+            $addin.save({
+                name: js_name,
+                data: resp.data,
+                handler: function () {
+                    $ui.alert({
+                        title: "安装完成",
+                        message: "\n是否打开？\n" + js_name,
+                        actions: [
+                            {
+                                title: "打开",
+                                handler: function () {
+                                    $app.openExtension(js_name)
+                                }
+                            },
+                            {
+                                title: "不了"
+                            }]
+                    });
+                }
             })
         }
     })
