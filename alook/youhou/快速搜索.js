e242fdd8f91dc129efba9245e16b926f8ef2c394
小良 <84088289@qq.com>
2022-11-11 22:25:19 +0800

    更新快速搜索油猴脚本

--- alook/youhou/快速搜索.js
+++ alook/youhou/快速搜索.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         快速搜索
 // @namespace    https://iphone8.vip/
-// @version      0.1
-// @description  在系统搜索框输入关键字+需要搜索的内容，即可跳转到对应的网站并搜索内容
+// @version      1.1
+// @description  在系统下拉搜索框输入关键字+需要搜索的内容，即可跳转到对应的网站并搜索内容
 // @author       iPhone8、小良
 // @match        *://m.baidu.com/*word=*
 // @icon         https://iphone8.vip/img/ssgjdq.jpg
@@ -18,6 +18,7 @@
     { 'name': '大师兄影视', 'Keyword': 'dsx', 'url': 'https://dsxys.pro/sb/ke7nhZe3c1-.html?wd=' }, 
     { 'name': '555电影', 'Keyword': '555', 'url': 'https://www.555dy.me/vodsearch/-------------.html?wd=' },
     { 'name': '爱奇艺', 'Keyword': 'iqy', 'url': 'https://m.iqiyi.com/search.html?source=input&key=' },
+    { 'name': '捷径音乐下载', 'Keyword': 'yy', 'url': 'shortcuts://run-shortcut?name=%E9%9F%B3%E4%B9%90%E4%B8%8B%E8%BD%BD1.6&input=' },
     { 'name': '高德导航', 'Keyword': 'dh', 'url': 'iosamap://poi?sourceApplication=pin&name=' }]
     
     const str = location.search
