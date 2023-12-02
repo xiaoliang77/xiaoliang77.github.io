4a79aa5eb27fe13f5882774e26441d9120f07f78
小良 <84088289@qq.com>
2023-12-02 20:06:35 +0800

    更新抖阴脚本

--- jsbox/js/douyin8.js
+++ jsbox/js/douyin8.js
@@ -1,13 +1,16 @@
 /*
-2022年9月18日、域名被墙请使用科学上网
-左右滑动记数有问题，需要复制视频链接和下载的朋友先往右边滑一个视频在滑动回来再操作下载和复制。这属于jsbox组件问题。
+2023年12月2日更新
+
+修复无法使用
+更换切换视频方式
+
+脚本仅供代码学习，请勿分享。非法传播造成法律问题与作者无关。
 
 by：iPhone 8、小良
 https://iphone8.vip/
 */
+
 var turl
-var video_url
-var key
 const gzgzh = {
   title: "关注公众号",
   handler: function () {
@@ -31,6 +34,7 @@ const gzgzh = {
 };
 
 const base64 = "aHR0cHM6Ly9pcGhvbmU4LnZpcC9jb25maWcvZG91eWluLmpzb24=";
+var js_name
 $ui.loading(true);
 $http.get({
   url: $text.base64Decode(base64),
@@ -38,15 +42,16 @@ $http.get({
     $ui.loading(false);
     if (resp.response.statusCode == "200") {
       var info = resp.data;
-      if (info.bb != "2.7.1") {
+      js_name = info.js_name
+      if (info.bb != "2.8") {
         $ui.alert({
           title: "温馨提示",
           message: info.gxsm,
           actions: [
             {
-              title: "访问官网",
+              title: "立即更新",
               handler: function () {
-                $app.openURL(info.gw);
+                download(info.updata)
               }
             },
             gzgzh
@@ -54,9 +59,7 @@ $http.get({
         });
       } else {
         turl = $text.base64Decode(info.turl)
-        key = $text.base64Decode(info.key)
-        getdata()
-
+        get_Video_data()
       }
     } else {
       $ui.alert("加载失败");
@@ -113,7 +116,8 @@ const conView = {
       }, events: {
         didSelect: function (sender, indexPath, data) {
           if (indexPath.row == 0) {
-            getdata()
+            $device.taptic(2)
+            get_Video_data()
           } else if (indexPath.row == 1) {
             $ui.alert("搜索功能暂未开发···");
           } else if (indexPath.row == 2) {
@@ -136,12 +140,11 @@ const conView = {
 };
 
 var timg = "https://iphone8.vip/"
-// var itemHeight = $device.info.screen.height;
 $ui.render({
   props: {
     id: "mView",
     navBarHidden: 1,
-    statusBarStyle:0,
+    statusBarStyle: 0,
     homeIndicatorHidden: 1,
     bgcolor: $color("black")
   },
@@ -155,26 +158,17 @@ $ui.render({
         {
           type: "label",
           props: {
-              id: "jzz",
-              font: $font("bold", 40),
-              textColor: $color("#fff"),
-              align: $align.center,
-              text: "加载中···"
+            id: "jzz",
+            font: $font("bold", 40),
+            textColor: $color("#fff"),
+            align: $align.center,
+            text: "加载中···"
           },
           layout: function (make, view) {
-              make.centerX.centerY.equalTo(view.center)
+            make.centerX.centerY.equalTo(view.center)
           }
-      },
-        {
-          type: "gallery",
-          props: {
-          },
-          layout: function (make, view) {
-            make.left.right.inset(-1)
-            make.top.inset(0)
-            make.bottom.inset(50)
-          }
-        }
+        },
+
       ],
       layout: $layout.fill,
     },
@@ -199,30 +193,29 @@ $ui.render({
       type: "button",
       props: {
         id: "xia_img",
-        src: timg + "jsbox/img/xia.png"
+        src: timg + "jsbox/img/drlogo.png",
+        radius: 25
       },
       events: {
         tapped: function (sender) {
-          // $ui.alert("该视频不支持下载，请复制视频地址到指尖浏览器上下载！");
-          download(video_url)
+          $ui.alert("暂无用户信息");
         }
       },
       layout: function (make, view) {
-        make.right.inset(2);
+        make.right.inset(5);
         make.bottom.equalTo($("x_img").top).inset(15);
-        make.width.height.equalTo(58);
+        make.width.height.equalTo(50);
       }
     },
     {
       type: "button",
       props: {
         id: "fuzhi_img",
-        src: timg + "jsbox/img/fuzhi.png"
+        src: timg + "jsbox/img/hongbao.png"
       },
       events: {
         tapped: function (sender) {
-          $clipboard.text = video_url;
-          $ui.toast("已复制");
+          $app.openURL("alipays://platformapi/startapp?saId=10000007&clientVersion=3.7.0.0718&qrcode=https%3A%2F%2Frender.alipay.com%2Fp%2Fc%2Falipay-red-qrcode%2Fshared.html%3Fchannel%3Dsearch_pwd%26shareId%3D2088202699097532%26token%3D19614922yglxkd7xgrvnf1fjlb%26campStr%3DkPPFvOxaCL3f85TiKss2wsBZgIjulHjG%26sign%3DqsiVOoa7TuphryWxyBdONXsMTnE3jiIBvWeUs3yV1sw%3D%26chInfo%3DDingtalk%26c_stype%3Dsearch_pwd%26code%3D798679953")
         }
       },
       layout: function (make, view) {
@@ -248,13 +241,11 @@ $ui.render({
         make.width.height.equalTo(50);
       }
     },
-    
     conView
   ]
 });
 
-
-function getdata() {
+function get_Video_data() {
   $http.get({
     url: turl,
     handler: function (resp) {
@@ -264,57 +255,81 @@ function getdata() {
         video_s.push({
           type: "video",
           props: {
-            id: "video" + i, src: data[i].videoUrl
-          }
-        })
+            id: "vid" + i,
+            src: data[i].videoUrl
+          },
+          layout: $layout.fill,
+        }
+        )
       }
-      video_url = data[0].videoUrl
-      var st = {
-        type: "gallery",
+      const st = {
+        type: "list",
         props: {
-          id: "div",
-          items: video_s,
-          interval: 0,
-          radius: 5.0
+          id: "listv",
+          data: video_s,
         },
         layout: function (make, view) {
-          make.left.right.inset(-1)
-          make.top.inset(0)
-          make.bottom.inset(50)
+          make.top.equalTo(0)
+          make.right.left.inset(0)
+          make.bottom.inset(90)
         },
         events: {
-          changed: function (sender) {
-            video_url = sender.viewWithIndex(sender.page).src;
-            if (sender.page == 4) {
-              getdata()
+          rowHeight: function (sender, indexPath) {
+            return sender.bounds.height
+          },
+          willEndDragging: function (sender, velocity, target) {
+            const liHeight = sender.bounds.height
+            let num = target.y / liHeight;
+            let integerPart = Math.floor(num);
+            let zx = integerPart * liHeight
+            let zd = zx + liHeight
+            let y = target.y
+            if (y < zd / 2) {
+              return $point(0, zx)
+            } else {
+              return $point(0, zd)
             }
-            $("video0").pause()
-            $("video1").pause()
-            $("video2").pause()
-            $("video3").pause()
-            $("video4").pause()
+          },
+          willBeginDragging: function (sender) {
+            sender.data.map(item => {
+              $(item.props.id).pause()
+            })
+          },
+          didReachBottom: function (sender) {
+            get_Video_data()
           }
         }
       }
       $("fenye").add(st)
-
     }
   });
-
 }
 
 function download(url) {
-  $ui.toast("正在下载中 ...");
-  $ui.loading(true);
+  $ui.toast("正在安装中 ...");
   $http.download({
     url: url,
-    handler: function(resp) {
-      $ui.loading(false);
-      if (resp.response.statusCode == "200") {
-        $share.sheet(["download.mp4", resp.data]);
-      } else {
-        $ui.alert("下载失败");
-      }
+    handler: function (resp) {
+      $addin.save({
+        name: js_name,
+        data: resp.data,
+        handler: function () {
+          $ui.alert({
+            title: "安装完成",
+            message: "\n是否打开？\n" + js_name,
+            actions: [
+              {
+                title: "打开",
+                handler: function () {
+                  $app.openExtension(js_name)
+                }
+              },
+              {
+                title: "不了"
+              }]
+          });
+        }
+      })
     }
-  });
+  })
 }
