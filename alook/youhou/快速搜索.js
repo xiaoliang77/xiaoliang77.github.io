ac93761682d26dda03d8879963f1254adcd2edac
小良 <84088289@qq.com>
2024-01-10 13:31:04 +0800

    更新 音乐下载 1.7

--- alook/youhou/快速搜索.js
+++ alook/youhou/快速搜索.js
@@ -5,6 +5,7 @@
 // @description  在系统下拉搜索框输入关键字+需要搜索的内容，即可跳转到对应的网站并搜索内容
 // @author       iPhone8、小良
 // @match        *://m.baidu.com/*word=*
+// @match        *://www.wuzhuiso.com/*
 // @icon         https://iphone8.vip/img/ssgjdq.jpg
 // @run-at       document-start
 // @grant        none
@@ -18,7 +19,7 @@
     { 'name': '大师兄影视', 'Keyword': 'dsx', 'url': 'https://dsxys.pro/sb/ke7nhZe3c1-.html?wd=' }, 
     { 'name': '555电影', 'Keyword': '555', 'url': 'https://www.555dy.me/vodsearch/-------------.html?wd=' },
     { 'name': '爱奇艺', 'Keyword': 'iqy', 'url': 'https://m.iqiyi.com/search.html?source=input&key=' },
-    { 'name': '捷径音乐下载', 'Keyword': 'yy', 'url': 'shortcuts://run-shortcut?name=%E9%9F%B3%E4%B9%90%E4%B8%8B%E8%BD%BD1.6&input=' },
+    { 'name': '捷径音乐下载', 'Keyword': 'yy', 'url': 'shortcuts://run-shortcut?name=%E9%9F%B3%E4%B9%90%E4%B8%8B%E8%BD%BD1.7&input=' },
     { 'name': '高德导航', 'Keyword': 'dh', 'url': 'iosamap://poi?sourceApplication=pin&name=' }]
     
     const str = location.search
