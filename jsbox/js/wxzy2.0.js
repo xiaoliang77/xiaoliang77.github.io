e99bac1ac8007f08833a04acf131729f0dbba982
小良 <84088289@qq.com>
2024-03-10 16:41:15 +0800

    更新无限资源脚本2.3版本

--- jsbox/js/wxzy2.0.js
+++ jsbox/js/wxzy2.0.js
@@ -14,7 +14,7 @@ const channelList = [
 const myHeaders = {
     "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1"
 };
-const urlt = $text.base64Decode("aHR0cHM6Ly9od213Lnlra2sudmlw");
+const urlt = $text.base64Decode("aHR0cHM6Ly9icXVkLmNzc3oudmlw");
 const mrhb = {
     type: "button",
     props: {
