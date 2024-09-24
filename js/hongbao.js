1fb747a7dd2d86b7e05ee35682d8b5ed54cf1e20
小良 <84088289@qq.com>
2024-09-24 23:19:56 +0800

    新增过度动画

--- js/hongbao.js
+++ js/hongbao.js
@@ -1,5 +1,5 @@
 (function () {
-    var hongbao = `<div style="position: fixed; bottom: 60px; right: 20px; z-index: 9999;">
+    var hongbao = `<div class="hongbao" style="position: fixed; bottom: 60px; right: 20px; z-index: 9999;">
     <a href="https://ae85-1251930860.cos.ap-chengdu.myqcloud.com/hongbao.html">
         <img  style="width: 60px; height: 60px; border-radius: 30px; box-shadow: 0 2px 4px 0 rgba(0,0,0,0.1), 0 4px 8px 0 rgba(0,0,0,0.1), 0 8px 16px 0 rgba(0,0,0,0.1), 0 16px 32px 0 rgba(0,0,0,0.1);" src="img/hb.jpg" />
     </a> 
